6dfd6c19bb160c84ec50488743814a5c7eb1918a
Alexandr <olexandrzhezheria@gmail.com>
2023-07-06 17:42:02 +0300

    fix urls.py in social_media_api fix views.py in user

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for social_media_api project.
+URL configuration for py_social_media_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/social/", include("social.urls", namespace="cinema")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
